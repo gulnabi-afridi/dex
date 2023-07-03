1a20c281a0a319c399d72dde408a30620b125d87
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-03 14:39:53 +0500

    solape token section completed

--- components/Home/SolapeToken/SolapeToken.tsx
+++ components/Home/SolapeToken/SolapeToken.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { PiCopyBold } from "react-icons/pi";
 import { Data } from "@/data/JSON";
+import Wrapper from "@/components/shared/ComponentWrapper/Wrapepr";
+import { PiCopyBold } from "react-icons/pi";
+import { AreaChart, Area, ResponsiveContainer } from "recharts";
 
 const SolapeToken = () => {
+  const [timeOptions, setTimeOptions] = useState([
+    {
+      active: true,
+      time: "24h",
+    },
+    {
+      active: false,
+      time: "1d",
+    },
+    {
+      active: false,
+      time: "7d",
+    },
+    {
+      active: false,
+      time: "30d",
+    },
+  ]);
+  const [selectedTime, setSelectedTime] = useState("24h");
+
+  const handelSetTimeOptions = (indexNumber: number) => {
+    const updatedTimeOptions = timeOptions.map((option, i) => {
+      if (i === indexNumber) {
+        setSelectedTime(option.time);
+        return {
+          ...option,
+          active: true,
+        };
+      } else {
+        return {
+          ...option,
+          active: false,
+        };
+      }
+    });
+
+    setTimeOptions(updatedTimeOptions);
+  };
+
   return (
     <div className="w-full flex flex-col">
       {/* ========> solape token */}
@@ -55,7 +96,133 @@ const SolapeToken = () => {
       </div>
 
       {/* =========> buy solape section */}
-      <div className="w-full "></div>
+      <Wrapper style="bg-th-gradient-2 py-16">
+        <div className="w-full grid grid-cols-[2fr,1.6fr] gap-20">
+          {/* left portion */}
+          <div className="w-full flex flex-col gap-6">
+            {/* top statistics  */}
+            <div className="w-full flex justify-between items-start">
+              {/*  */}
+              <div className="flex flex-col gap-3 items-start">
+                <p className="text-th-brand leading-[70px] text-[56px] font-medium font-inter">
+                  $0.5424
+                </p>
+                <div className="flex gap-3 justify-center items-center">
+                  <p className="text-th-brand px-[4px] py-[1px] flex justify-center items-center rounded-[4px] text-[14px] font-inter font-normal bg-th-brand-secondary">
+                    {selectedTime}
+                  </p>
+                  <p className="text-[18px] font-normal font-inter text-th-fkg-1">
+                    +$0.0042 (+1.44%)
+                  </p>
+                </div>
+              </div>
+              {/*  */}
+              <div className="flex justify-center items-center gap-3">
+                {timeOptions.map((item, index) => {
+                  return (
+                    <button
+                      onClick={() => handelSetTimeOptions(index)}
+                      key={index}
+                      className={`px-[6px] py-[3px] text-th-brand rounded-[4px] text-[14px] font-inter font-normal ${
+                        item.active ? "bg-th-brand-secondary" : "bg-th-fkg-3"
+                      } `}
+                    >
+                      {item.time}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+            {/* =====> cart */}
+            <div className="w-full h-[280px]">
+              <ResponsiveContainer width="99%" height="100%">
+                <AreaChart
+                  data={Data.solapeTokenChartData}
+                  margin={{
+                    top: 5,
+                    right: 0,
+                    left: 0,
+                    bottom: 5,
+                  }}
+                >
+                  <Area
+                    dataKey="value"
+                    stroke="#ff9735"
+                    strokeWidth={2}
+                    fill="#603e1b"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+          {/* right portion */}
+          <div className="w-full max-w-[450px] flex flex-col gap-8">
+            {/* Market Cap Rank + Market Cap */}
+            <div className="w-full grid grid-cols-2 justify-center items-center">
+              <div className="flex flex-col gap-2">
+                <p className="text-[20px] font-medium font-inter text-th-fkg-4">
+                  Market Cap Rank
+                </p>
+                <p className="text-[32px] font-medium font-inter text-th-brand">
+                  #84
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-[20px] font-medium font-inter text-th-fkg-4">
+                  Market Cap
+                </p>
+                <p className="text-[32px] font-medium font-inter text-th-brand">
+                  $274.94B
+                </p>
+              </div>
+            </div>
+            {/* YTD ROI + 24h Volume */}
+            <div className="w-full grid grid-cols-2 justify-center items-start">
+              <div className="flex flex-col  gap-2">
+                <p className="text-[20px] font-medium font-inter text-th-fkg-4">
+                  YTD ROI
+                </p>
+                <p className="text-[32px] font-medium font-inter text-th-brand">
+                  $30.6M
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-[20px] font-medium font-inter text-th-fkg-4">
+                  24h Volume
+                </p>
+                <p className="text-[32px] font-medium font-inter text-th-brand">
+                  $44.2M
+                </p>
+              </div>
+            </div>
+            {/* Circulating Supply */}
+            <div className="w-full grid grid-cols-2 justify-center items-start">
+              <div className="flex flex-col  gap-2">
+                <p className="text-[20px] font-medium font-inter text-th-fkg-4">
+                  Circulating Supply
+                </p>
+                <p className="text-[32px] font-medium font-inter text-th-brand">
+                  $30.6M
+                </p>
+              </div>
+            </div>
+            {/* ===> buy solape button */}
+            <button className="w-[288px] h-[72px] border-[3px] border-th-brand-secondary rounded-[12px] flex justify-center items-center gap-3">
+              <p className="text-[24px] font-semibold font-inter text-th-brand">
+                Buy SOLAPE
+              </p>
+              <div className="w-[32px] h-[32px] flex justify-center items-center">
+                <Image
+                  src="/assets/sloapeToken.svg"
+                  width={22}
+                  height={21}
+                  alt=""
+                />
+              </div>
+            </button>
+          </div>
+        </div>
+      </Wrapper>
     </div>
   );
 };
--- data/JSON.js
+++ data/JSON.js
@@ -107,4 +107,62 @@ export const Data = {
       height: 25,
     },
   ],
+  solapeTokenChartData: [
+    {
+      name: "price 1",
+      value: 722,
+    },
+    {
+      name: "price 1",
+      value: 1722,
+    },
+    {
+      name: "price 2",
+      value: 330,
+    },
+    {
+      name: "price 2",
+      value: 1330,
+    },
+    {
+      name: "price 2",
+      value: 830,
+    },
+    {
+      name: "price 2",
+      value: 1430,
+    },
+    {
+      name: "price 2",
+      value: 2030,
+    },
+    {
+      name: "price 2",
+      value: 1470,
+    },
+    {
+      name: "price 2",
+      value: 1090,
+    },
+    {
+      name: "price 2",
+      value: 1890,
+    },
+    {
+      name: "price 2",
+      value: 1990,
+    },
+    {
+      name: "price 2",
+      value: 890,
+    },
+    {
+      name: "price 2",
+      value: 1090,
+    },
+    {
+      name: "price 2",
+      value: 1460,
+    },
+  ],
 };
