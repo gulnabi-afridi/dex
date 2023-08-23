03f52352ee8e9d1a816d0c379c3cf6d62c42eaeb
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-23 12:56:42 +0500

    structure for svgs icons ready

--- views/Dex/Dex.tsx
+++ views/Dex/Dex.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import WideWrapper from "@/components/shared/ComponentWrapper/WideWrapper";
-import { AiOutlineStar } from "react-icons/ai";
+import { HiOutlineStar } from "react-icons/hi";
+import { HiStar } from "react-icons/hi";
+import * as Icons from "../../SVG/Icons";
 
 const Dex = () => {
+  // states ------------->
+  const [isFavorite, setIsFavorite] = useState(false);
+  // states ends here ----------------->
+
   return (
     <WideWrapper style="bg-black">
       <div className="w-full h-[100px] flex justify-between items-center">
         {/* left portion --------------> */}
         <div className="flex justify-center items-center gap-2">
           {/* add to favorite icon */}
+          {isFavorite ? (
+            <HiOutlineStar
+              onClick={() => setIsFavorite(!isFavorite)}
+              className="text-white text-[28px] cursor-pointer"
+            />
+          ) : (
+            <HiStar
+              onClick={() => setIsFavorite(!isFavorite)}
+              className="text-white text-[32px] cursor-pointer"
+            />
+          )}
+          {/* coin name */}
+          <div className="flex justify-center items-center">
+            <Icons.SolapeCoin className="w-[28px] h-[28px]" />
+          </div>
         </div>
       </div>
     </WideWrapper>
