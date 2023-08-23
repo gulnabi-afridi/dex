75fe2ed2118bb41a824b9abe86b7797f223e2f9b
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-23 18:40:27 +0500

    slider card for dex page developed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import "@/styles/pagination.css";
 import type { AppProps } from "next/app";
 import Layout from "@/layout/Layout";
 import { ThemeProvider } from "next-themes";
+// ==> slick styles
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
