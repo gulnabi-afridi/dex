36bab7b51e66b01aa3db57a29505e61ae3581850
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-28 14:26:38 +0500

    recent trades table component also developed

--- views/Dex/Dex.tsx
+++ views/Dex/Dex.tsx
@@ -4,11 +4,10 @@ import { HiOutlineStar } from "react-icons/hi";
 import { BsChevronDown, BsUbuntu } from "react-icons/bs";
 import { PiTriangleFill } from "react-icons/pi";
 import { PiCaretRightBold } from "react-icons/pi";
-import FillButton from "@/components/shared/Buttons/FillButton";
 import AssetInformationWidget from "@/components/Dex/AssetInformationWidget";
 import AccountsWidget from "@/components/Dex/AccountsWidget";
 import ConnectWalletWidget from "@/components/Dex/ConnectWalletWidget";
-import { Data } from "@/data/JSON";
+import RecentTradesTable from "@/components/Dex/RecentTradesTable";
 import * as Icons from "../../SVG/Icons";
 import Link from "next/link";
 import Image from "next/image";
@@ -199,71 +198,7 @@ const Dex = () => {
           {/* connect wallet --------->  */}
           <ConnectWalletWidget />
           {/* recent trades ---------> */}
-          <div className="w-full flex flex-col bg-th-bkg-4 px-4 py-5 rounded-md">
-            <p className="text-[14px] text-center font-inter font-normal text-white">
-              Recent Trades
-            </p>
-            {/* table -------->  */}
-            <div className="w-full flex flex-col gap-3 mt-5">
-              {/* table column --> */}
-              <div className="w-full grid grid-cols-[1.5fr,1.5fr,1fr,1fr]">
-                {/* ist col label */}
-                <div className="w-full flex justify-start items-center">
-                  <p className="text-[14px] font-normal text-th-fkg-4">
-                    Price (USDC)
-                  </p>
-                </div>
-                {/* 2nd col label */}
-                <div className="w-full flex justify-end items-center">
-                  <p className="text-[14px] font-normal text-th-fkg-4">
-                    Size (SOLAPE)
-                  </p>
-                </div>
-                {/* 3rd col label */}
-                <div className="w-full flex justify-end items-center">
-                  <p className="text-[14px] font-normal text-th-fkg-4">Tx</p>
-                </div>
-                {/* 4rth col label */}
-                <div className="w-full flex justify-end items-center">
-                  <p className="text-[14px] font-normal text-th-fkg-4">Time</p>
-                </div>
-              </div>
-              {/* table rows --> */}
-              <div className="w-full max-h-[285px] overflow-auto flex flex-col gap-2 hideScrollBar">
-                {Data.dexData.recentTradesData.map((item, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="w-full grid grid-cols-[1.5fr,1.5fr,1fr,1fr]"
-                    >
-                      {/* price */}
-                      <div className="w-full flex justify-start items-center">
-                        <p className="text-[14px] text-[#0AD171] font-normal font-inter">
-                          {item.price}
-                        </p>
-                      </div>
-                      {/* size */}
-                      <div className="w-full flex justify-end items-center">
-                        <p className="text-[14px] text-white font-normal font-inter">
-                          {item.size}
-                        </p>
-                      </div>
-                      {/* tx */}
-                      <div className="w-full flex justify-end items-center">
-                        <Icons.Link />
-                      </div>
-                      {/* time */}
-                      <div className="w-full flex justify-end items-center">
-                        <p className="text-[14px] text-white font-normal font-inter">
-                          {item.time}
-                        </p>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
+          <RecentTradesTable />
         </div>
       </div>
     </WideWrapper>
